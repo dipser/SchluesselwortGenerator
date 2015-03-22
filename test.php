ae848286bcdf5b4a19f6a974fa89edf7e614fdec
Aurel Hermand <github@devone.de>
2015-03-22 02:49:53 +0100

    Update test.php

--- test.php
+++ test.php
@@ -7,21 +7,15 @@ $data = [
 ];
 
 
-// 1. Finde Einträge...
+// Finde Einträge...
 // Ist es möglich mithilfe von $selects automatisch $data zu durchsuchen und die passenden Einträge zu finden?
-// Alle möglichen Repräsentationen müssten getestet werden.
+// Alle möglichen Repräsentationen müssten getestet werden. => Repräsentationen mit PHP generieren und abfragen.
 $selects = [
-  '11.11.2015'
-];
-
-
-// 2. Finde Einträge...
-// Ist es möglich mithilfe von $selects automatisch $data zu durchsuchen und die passenden Einträge zu finden?
-// Alle möglichen Repräsentationen müssten getestet werden.
-$selects = [
+  '11.11.2015',
   '*',
   '*/2'
 ];
+
 
 
 
