8867d0d990334ad21ad88fe4086006b762a0cbc3
Aurel Hermand <github@devone.de>
2015-03-22 02:48:36 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,35 @@
+<?php
 
+// DB-Einträge der Cronjobs
+$data = [
+  '*',
+  '*/4'
+];
+
+
+// 1. Finde Einträge...
+// Ist es möglich mithilfe von $selects automatisch $data zu durchsuchen und die passenden Einträge zu finden?
+// Alle möglichen Repräsentationen müssten getestet werden.
+$selects = [
+  '11.11.2015'
+];
+
+
+// 2. Finde Einträge...
+// Ist es möglich mithilfe von $selects automatisch $data zu durchsuchen und die passenden Einträge zu finden?
+// Alle möglichen Repräsentationen müssten getestet werden.
+$selects = [
+  '*',
+  '*/2'
+];
+
+
+
+
+// Regex
+$regex = '';
+
+
+
+
+?>
